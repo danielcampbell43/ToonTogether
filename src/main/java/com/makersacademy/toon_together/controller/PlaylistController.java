670c89715fe0df3090703aa642c6440504f5feaf
danielcampbell43 <danielsgiveawayemail@icloud.com>
2024-06-18 12:05:10 +0100

    Added ability to create a playlist - just probably not how we want it

--- src/main/java/com/makersacademy/toon_together/controller/PlaylistController.java
+++ src/main/java/com/makersacademy/toon_together/controller/PlaylistController.java
@@ -5,9 +5,13 @@ import com.makersacademy.toon_together.repository.AuthoritiesRepository;
 import com.makersacademy.toon_together.repository.PlaylistRepository;
 import com.makersacademy.toon_together.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class PlaylistController {
@@ -22,7 +26,16 @@ public class PlaylistController {
     public String index(Model model) {
 
         Iterable<Playlist> playlists = playlistRepository.findAll();
+        model.addAttribute("playlist", new Playlist());
         model.addAttribute("playlists", playlists);
         return "/index";
+    }
+
+    @PostMapping("/playlists")
+    public RedirectView create(@ModelAttribute Playlist playlist, Authentication auth) {
+        playlist.setUser(userRepository.findByUsername(auth.getName()));
+        playlistRepository.save(playlist);
+        return new RedirectView("/playlists");
+
     }
 }
--- src/main/java/com/makersacademy/toon_together/repository/UserRepository.java
+++ src/main/java/com/makersacademy/toon_together/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.makersacademy.toon_together.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    User findByUsername(String name);
 }
