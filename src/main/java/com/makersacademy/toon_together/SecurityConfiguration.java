8374d45c2af9045fa9d8bf7e5cf27f79e557929c
bethany <bethany.hall66@gmail.com>
2024-06-19 14:20:03 +0100

    fixed tests and copied in security config info for playlists and myprofile

--- src/main/java/com/makersacademy/toon_together/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/toon_together/SecurityConfiguration.java
@@ -27,6 +27,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").hasRole("USER")
+                .antMatchers("/playlists").hasRole("USER")
+                .antMatchers("/myprofile").hasRole("USER")
                 .antMatchers("/users", "/login", "/logout").permitAll()
                 .and()
                 .formLogin()
