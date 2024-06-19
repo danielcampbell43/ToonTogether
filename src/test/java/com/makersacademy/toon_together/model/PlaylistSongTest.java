d326c618344de0ae1bd4a98486a8d4469d6b2063
danielcampbell43 <danielsgiveawayemail@icloud.com>
2024-06-19 16:31:03 +0100

    Added tests for PlaylistSong model and modified model to include createdAt

---
+++ src/test/java/com/makersacademy/toon_together/model/PlaylistSongTest.java
@@ -0,0 +1,40 @@
+package com.makersacademy.toon_together.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PlaylistSongTest {
+    public Playlist playlist;
+    public Song song;
+    public PlaylistSong playlistSong;
+
+    @Before
+    public void setUp() {
+        playlist = mock(Playlist.class);
+        when(playlist.getId()).thenReturn(1);
+        song = mock(Song.class);
+        when(song.getId()).thenReturn(1);
+        playlistSong = new PlaylistSong(playlist, song, new Timestamp(10000));
+    }
+
+    @Test
+    public void playlistSongHasPlaylist() {
+        assertEquals(playlistSong.getPlaylist().getId(), 1);
+    }
+
+    @Test
+    public void playlistSongHasSong() {
+        assertEquals(playlistSong.getSong().getId(), 1);
+    }
+
+    @Test
+    public void playlistSongHasCreateedAt() {
+        assertEquals(playlistSong.getCreatedAt(), new Timestamp(10000));
+    }
+}
