2497c0d96c44be73a300d8e125d41cb4eb375b0a
danielcampbell43 <danielsgiveawayemail@icloud.com>
2024-06-19 15:57:35 +0100

    Added tests for songs, made changes to song model

---
+++ src/test/java/com/makersacademy/toon_together/model/SongTest.java
@@ -0,0 +1,23 @@
+package com.makersacademy.toon_together.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+
+public class SongTest {
+
+    public Song song;
+
+    @Before
+    public void setUp() {
+        song = new Song(new Timestamp(10000));
+    }
+
+    @Test
+    public void songHasCreatedAt() {
+        assertEquals(song.getCreatedAt(), new Timestamp(10000));
+    }
+}
