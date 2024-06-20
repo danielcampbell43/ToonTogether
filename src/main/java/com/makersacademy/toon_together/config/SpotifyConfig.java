ba8eb84c0979c7c0039390780029bfe2b5111623
alex-nazemi <alexandernazemi@gmail.com>
2024-06-21 00:07:57 +0100

    changed property source for spotify config to application-secret.properties file

--- src/main/java/com/makersacademy/toon_together/config/SpotifyConfig.java
+++ src/main/java/com/makersacademy/toon_together/config/SpotifyConfig.java
@@ -3,9 +3,11 @@ package com.makersacademy.toon_together.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import se.michaelthelin.spotify.SpotifyApi;
 
 @Configuration
+@PropertySource("classpath:application-secret.properties") // Specify your secret properties file
 public class SpotifyConfig {
 
     @Value("${spotify.access-token}")
