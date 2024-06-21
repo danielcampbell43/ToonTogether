9176d1b87aede48f099e6afe5708367dea765182
Charlie Giaretta <charliegiaretta@outlook.com>
2024-06-21 12:17:23 +0100

    Add pagination to /playlists

--- src/main/java/com/makersacademy/toon_together/repository/PlaylistRepository.java
+++ src/main/java/com/makersacademy/toon_together/repository/PlaylistRepository.java
@@ -2,6 +2,8 @@ package com.makersacademy.toon_together.repository;
 
 import com.makersacademy.toon_together.model.Playlist;
 import com.makersacademy.toon_together.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,6 +12,6 @@ public interface PlaylistRepository extends CrudRepository<Playlist, Integer> {
     List<Playlist> findByOwner(User user);
     Playlist findById(int id);
     void deleteById(int id);
-    List<Playlist> findByNameContainingIgnoreCase(String name);
-    List<Playlist> findAllByOrderByCreatedAtDesc();
+    Page<Playlist> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Playlist> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
