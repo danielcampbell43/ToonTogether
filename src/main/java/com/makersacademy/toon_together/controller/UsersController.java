cd37e95e17b24fc2283e7c954ce3b48bb5e26d0b
danielcampbell43 <danielsgiveawayemail@icloud.com>
2024-06-21 15:18:59 +0100

    Added feature to salt and hash passwords before storage

--- src/main/java/com/makersacademy/toon_together/controller/UsersController.java
+++ src/main/java/com/makersacademy/toon_together/controller/UsersController.java
@@ -5,6 +5,7 @@ import com.makersacademy.toon_together.model.User;
 import com.makersacademy.toon_together.repository.AuthoritiesRepository;
 import com.makersacademy.toon_together.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.security.SecureRandom;
+
 @Controller
 public class UsersController {
 
+    public final static int STRENGTH = 10;
     @Autowired
     UserRepository userRepository;
     @Autowired
     AuthoritiesRepository authoritiesRepository;
+
+    public static String encodePassword(String plainPassword) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(STRENGTH, new SecureRandom());
+        return bCryptPasswordEncoder.encode(plainPassword);
+    }
 
     @GetMapping("/users/new")
     public String signup(Model model) {
@@ -29,6 +38,7 @@ public class UsersController {
     @PostMapping("/users")
     public RedirectView signup(@ModelAttribute User user) {
         user.setProfilePicture("/default-profile.jpg");
+        user.setPassword(encodePassword(user.getPassword()));
         userRepository.save(user);
         Authority authority = new Authority(user.getUsername(), "ROLE_USER");
         authoritiesRepository.save(authority);
