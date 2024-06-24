bfc8ad7d89a06056b2af844aca3fe0aca0f4662c
emilyhardy98 <hardye8@gmail.com>
2024-06-24 11:47:22 +0100

    updated table

--- src/main/resources/db/migration/V8__adding_collaborator_table.sql
+++ src/main/resources/db/migration/V8__adding_collaborator_table.sql
@@ -4,4 +4,6 @@ CREATE TABLE collaborators (
     playlist_id BIGINT NOT NULL,
     deleter BOOLEAN NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CONSTRAINTS fk_collaborators_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINTS fk_collaborators_playlist FOREIGN KEY (playlist_id) REFERENCES playlists(id)
 );
