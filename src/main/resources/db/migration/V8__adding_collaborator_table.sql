130bfd5b98bfb8498c0046233d2af1d2c94266f9
emilyhardy98 <hardye8@gmail.com>
2024-06-24 11:53:11 +0100

    v8 correct

--- src/main/resources/db/migration/V8__adding_collaborator_table.sql
+++ src/main/resources/db/migration/V8__adding_collaborator_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE collaborators (
     id BIGINT PRIMARY KEY,
     user_id BIGINT NOT NULL,
     playlist_id BIGINT NOT NULL,
-    deleter BOOLEAN NOT NULL DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    CONSTRAINTS fk_collaborators_user FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINTS fk_collaborators_playlist FOREIGN KEY (playlist_id) REFERENCES playlists(id)
+    deleter BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_collaborators_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_collaborators_playlist FOREIGN KEY (playlist_id) REFERENCES playlists(id)
 );
