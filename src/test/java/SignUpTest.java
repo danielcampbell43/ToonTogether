b4f2de8bd8509280ac4b6677c50254a93bd88a82
bethany <bethany.hall66@gmail.com>
2024-06-24 15:54:32 +0100

    Try fixing tests/sign up success message

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -8,8 +8,12 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.Duration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -32,10 +36,12 @@ public class SignUpTest {
 
     @Test
     public void successfulSignUpRedirectsToSignIn() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get("http://localhost:8080/users/new");
         driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
         driver.findElement(By.id("password")).sendKeys("password");
         driver.findElement(By.id("submit")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/login")));
         String title = driver.getTitle();
         Assert.assertEquals("Please sign in", title);
     }
