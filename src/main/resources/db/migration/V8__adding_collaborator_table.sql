5c90a2d195779fee17cf4f167e8056f4a4d06ddc
emilyhardy98 <hardye8@gmail.com>
2024-06-24 11:30:42 +0100

    v8 file added

---
+++ src/main/resources/db/migration/V8__adding_collaborator_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE collaborators (
+    id BIGINT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    playlist_id BIGINT NOT NULL,
+    deleter BOOLEAN NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
