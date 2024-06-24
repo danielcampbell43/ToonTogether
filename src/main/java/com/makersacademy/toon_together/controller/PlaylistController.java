c12bd72feaa5ace64ce5a77b1c0139c5da416122
bethany <bethany.hall66@gmail.com>
2024-06-24 13:51:50 +0100

    Success message for login/logout functional, success message for signup not yet working

--- src/main/java/com/makersacademy/toon_together/controller/PlaylistController.java
+++ src/main/java/com/makersacademy/toon_together/controller/PlaylistController.java
@@ -15,6 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class PlaylistController {
     @Autowired
@@ -27,7 +30,8 @@ public class PlaylistController {
     @GetMapping("/playlists")
     public String index(@RequestParam(value = "search", required = false) String search, Model model, Authentication auth,
                         @RequestParam(defaultValue = "0") int page,
-                        @RequestParam(defaultValue = "10") int size) {
+                        @RequestParam(defaultValue = "10") int size,
+                        HttpServletRequest request) {
         User user = userRepository.findByUsername(auth.getName());
         Pageable pageable = PageRequest.of(page, size);
         Page<Playlist> playlistPage;
@@ -45,6 +49,13 @@ public class PlaylistController {
         model.addAttribute("currentUser", user);
         model.addAttribute("totalPages", playlistPage.getTotalPages());
         model.addAttribute("currentPage", page);
+
+        HttpSession session = request.getSession();
+        String loginMessage = (String) session.getAttribute("loginMessage");
+        if (loginMessage != null) {
+            model.addAttribute("loginMessage", loginMessage);
+            session.removeAttribute("loginMessage");
+        }
 
         return "/index";
     }
