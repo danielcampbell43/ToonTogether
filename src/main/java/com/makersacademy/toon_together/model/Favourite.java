d42f6a927f2707e3218d1b50e5c06a0955549ac7
danielcampbell43 <danielsgiveawayemail@icloud.com>
2024-06-24 12:43:41 +0100

    Added model for favourite

---
+++ src/main/java/com/makersacademy/toon_together/model/Favourite.java
@@ -0,0 +1,34 @@
+package com.makersacademy.toon_together.model;
+
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@Entity
+@Table(name = "favourites")
+@NoArgsConstructor
+public class Favourite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "playlistId")
+    private Playlist playlist;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private java.sql.Timestamp createdAt;
+
+    public Favourite(Playlist playlist, User user, java.sql.Timestamp createdAt) {
+        this.playlist = playlist;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
+}
