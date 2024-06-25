827d4d715042c13bfc07eb4580f39aee8fd96746
alex-nazemi <alexandernazemi@gmail.com>
2024-06-25 12:28:47 +0100

    added saving user input, retrieving track data from spotify api, storing track data in database

--- src/main/java/com/makersacademy/toon_together/controller/SpotifyController.java
+++ src/main/java/com/makersacademy/toon_together/controller/SpotifyController.java
@@ -1,43 +1,57 @@
 package com.makersacademy.toon_together.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.makersacademy.toon_together.model.Song;
+import com.makersacademy.toon_together.repository.SongRepository;
 import com.makersacademy.toon_together.service.SpotifyService;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
 import se.michaelthelin.spotify.model_objects.specification.Track;
-import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class SpotifyController {
 
-    // http://localhost:8080/track/sync/6rqhFgbbKwnb9MLmUQDhG6
-
     @Autowired
     private SpotifyService spotifyService;
+
+    @Autowired
+    private SongRepository songRepository;
 
     // Handle form submission
     @PostMapping("/addSong")
     public String addSong(@RequestParam("songName") String songName, Model model) {
-        // Call the Spotify service to get track data
         try {
             Track track = spotifyService.searchTrackByName(songName);
             model.addAttribute("track", track);
-            System.out.println(track);
+
             StringBuilder artists = new StringBuilder();
             for (ArtistSimplified artist : track.getArtists()) {
                 artists.append(artist.getName()).append(", ");
             }
             String artistNames = artists.substring(0, artists.length() - 2);
-            // Remove the trailing comma and space
-            System.out.println(track.getName());
-            System.out.println(artistNames);
-            // json
-            // name, artist, album, release_year, image
+
+            // Create Song object with individual fields
+            Song song = new Song(
+                    track.getId(),
+                    track.getName(),
+                    artistNames,
+                    track.getAlbum().getName(),
+                    track.getAlbum().getReleaseDate(),
+                    track.getAlbum().getImages()[0].getUrl()
+            );
+
+            songRepository.save(song);
+
             return "termsandconditions";
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             e.printStackTrace();
